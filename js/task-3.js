ab0dd5207551b57029178cf1cec8d85e59242a7f
Serhii <sir3720@gmail.com>
2025-03-08 20:07:45 +0200

    add html js

---
+++ js/task-3.js
@@ -0,0 +1,42 @@
+/**
+ * Арифметичні функції
+ *
+ * - Number.parseInt
+ * - Number.parseFloat
+ * - Math
+ */
+
+//!======================================================
+// const x = parseFloat('2.5.6rem');
+
+// console.log(x);
+
+// let elementWidth = '50px';
+// console.log('elementWidth: ', parseInt(elementWidth));
+
+// let elementHeight = '200.74px';
+// console.log('elementHeight: ', parseFloat(elementHeight));
+
+//!======================================================
+
+// Math.round(5.49); // 6
+// Math.ceil(5.1); // 6
+// Math.floor(5.8); //5
+
+// Math.random(); // 0 - 1;
+
+// const rand = Math.round(Math.random() * 1000) + 500;
+// console.log(rand);
+
+//!======================================================
+/**
+ * Напиши скрипт, який виводить у консоль заокруглені вгору/вниз і т.д.
+ * значення змінної value. Використовуй методи Math.floor(), Math.ceil()
+ * та Math.round(). Перевір що буде в консолі при значеннях 27.3 та 27.9.
+ */
+
+// const value = 27.9;
+
+// console.log(Math.ceil(value));
+// console.log(Math.floor(value));
+// console.log(Math.round(value));
